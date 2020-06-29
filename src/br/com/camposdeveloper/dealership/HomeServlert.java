787b3971214b77961bef9723db767a518cf56d06
Willie Campos Silva <campos.developer@gmail.com>
2020-06-29 20:12:25 -0300

    New servlet with html code in java code.

---
+++ src/br/com/camposdeveloper/dealership/HomeServlert.java
@@ -0,0 +1,37 @@
+package br.com.camposdeveloper.dealership;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/home")
+public class HomeServlert extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("<html>");
+		sb.append("<body>");
+		sb.append("<div align='center' >");
+		sb.append("<span>HomeServlet!</span>");
+		sb.append("</div>");
+		sb.append("</body>");
+		sb.append("</html>");
+		
+		PrintWriter writer = response.getWriter();
+		writer.write(sb.toString());
+		writer.flush();
+		writer.close();
+		
+	}
+
+
+}
